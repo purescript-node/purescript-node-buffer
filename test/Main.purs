9d09e4dc82ecd732ea5f110bdb986c84f0a3e29a
Harry Garrood <harry@garrood.me>
2015-11-10 17:49:12 +0100

    Add some basic tests

--- test/Main.purs
+++ test/Main.purs
@@ -2,7 +2,49 @@ module Test.Main where
 
 import Prelude
 import Control.Monad.Eff
+import Control.Monad.Eff.Console (log)
+import Test.Assert
 
--- TODO
-main :: forall e. Eff e Unit
-main = return unit
+import Node.Buffer
+
+main :: Eff _ Unit
+main = do
+  log "Testing..."
+  log "Reading and writing"
+  testReadWrite
+  log "fromArray"
+  testFromArray
+  log "toArray"
+  testToArray
+
+testReadWrite :: Eff _ Unit
+testReadWrite = do
+  buf <- create 1
+  let val = 42
+  write UInt8 val 0 buf
+  readVal <- read UInt8 0 buf
+
+  assertEq val readVal
+
+testFromArray :: Eff _ Unit
+testFromArray = do
+  buf <- fromArray [1,2,3,4,5]
+  readVal <- read UInt8 2 buf
+
+  assertEq 3 readVal
+
+testToArray :: Eff _ Unit
+testToArray = do
+  let val = [1,2,67,3,3,7,8,3,4,237]
+
+  buf    <- fromArray val
+  valOut <- toArray buf
+
+  assertEq val valOut
+
+assertEq :: forall a. (Eq a, Show a) => a -> a -> Eff _ Unit
+assertEq x y =
+  if x == y
+    then return unit
+    else let msg = show x ++ " and " ++ show y ++ " were not equal."
+         in assert' msg false
