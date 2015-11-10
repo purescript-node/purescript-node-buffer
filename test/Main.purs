29d53dc9631d0616ea2662d63e263a664d0adeab
Harry Garrood <harry@garrood.me>
2015-11-10 17:58:46 +0100

    More tests

--- test/Main.purs
+++ test/Main.purs
@@ -2,22 +2,37 @@ module Test.Main where
 
 import Prelude
 import Control.Monad.Eff
-import Control.Monad.Eff.Console (log)
+import Control.Monad.Eff.Console (log, CONSOLE())
 import Test.Assert
 
 import Node.Buffer
+import Node.Encoding
 
-main :: Eff _ Unit
+type Test = forall e. Eff (assert :: ASSERT, buffer :: BUFFER, console :: CONSOLE | e) Unit
+
+main :: Test
 main = do
   log "Testing..."
+
   log "Reading and writing"
   testReadWrite
+
   log "fromArray"
   testFromArray
+
   log "toArray"
   testToArray
 
-testReadWrite :: Eff _ Unit
+  log "fromString"
+  testFromString
+
+  log "toString"
+  testToString
+
+  log "readString"
+  testReadString
+
+testReadWrite :: Test
 testReadWrite = do
   buf <- create 1
   let val = 42
@@ -26,14 +41,14 @@ testReadWrite = do
 
   assertEq val readVal
 
-testFromArray :: Eff _ Unit
+testFromArray :: Test
 testFromArray = do
   buf <- fromArray [1,2,3,4,5]
   readVal <- read UInt8 2 buf
 
   assertEq 3 readVal
 
-testToArray :: Eff _ Unit
+testToArray :: Test
 testToArray = do
   let val = [1,2,67,3,3,7,8,3,4,237]
 
@@ -42,7 +57,34 @@ testToArray = do
 
   assertEq val valOut
 
-assertEq :: forall a. (Eq a, Show a) => a -> a -> Eff _ Unit
+testFromString :: Test
+testFromString = do
+  let str = "hello, world"
+
+  buf <- fromString str ASCII
+  val <- read UInt8 6 buf
+
+  assertEq val 32 -- ASCII space
+
+testToString :: Test
+testToString = do
+  let str = "hello, world"
+
+  buf    <- fromString str ASCII
+  strOut <- toString ASCII buf
+
+  assertEq str strOut
+
+testReadString :: Test
+testReadString = do
+  let str = "hello, world"
+
+  buf    <- fromString str ASCII
+  strOut <- readString ASCII 7 12 buf
+
+  assertEq "world" strOut
+
+assertEq :: forall a. (Eq a, Show a) => a -> a -> Test
 assertEq x y =
   if x == y
     then return unit
