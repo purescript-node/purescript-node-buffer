fab1be56fed65f6091917cc94d6632d6317ebed7
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-06-23 18:00:13 -0500

    Add missing semicolons

--- src/Node/Buffer/Immutable.js
+++ src/Node/Buffer/Immutable.js
@@ -13,11 +13,11 @@ export const size = (buff) => buff.length;
 export const toArray = (buff) => {
   var json = buff.toJSON();
   return json.data || json;
-}
+};
 
 export const toArrayBuffer = (buff) => {
   return buff.buffer.slice(buff.byteOffset, buff.byteOffset + buff.byteLength);
-}
+};
 
 export const fromArrayBuffer = (ab) => Buffer.from(ab);
 export const fromStringImpl = (str, encoding) => Buffer.from(str, encoding);
