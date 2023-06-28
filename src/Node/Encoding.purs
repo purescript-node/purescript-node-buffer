7bb1160384f5e95fd5a487e68f17c121b1fda2cf
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-06-28 18:02:44 -0500

    Breaking: remove aliases; add Base64Url

--- src/Node/Encoding.purs
+++ src/Node/Encoding.purs
@@ -1,3 +1,5 @@
+-- | Note: encodings that are aliases of another one
+-- | are not supported below as a way to prevent confusion.
 module Node.Encoding
   ( Encoding(..)
   , encodingToNode
@@ -11,21 +13,21 @@ import Data.Function.Uncurried (Fn2, runFn2)
 data Encoding
   = ASCII
   | UTF8
+  -- | Note: the `ucs2` and `ucs-2` encodings are aliases to this one.
   | UTF16LE
-  | UCS2
   | Base64
+  | Base64Url
+  -- | Note: the `binary` encoding is an alias to this one.
   | Latin1
-  | Binary
   | Hex
 
 instance showEncoding :: Show Encoding where
   show ASCII = "ASCII"
   show UTF8 = "UTF8"
   show UTF16LE = "UTF16LE"
-  show UCS2 = "UCS2"
   show Base64 = "Base64"
+  show Base64Url = "Base64Url"
   show Latin1 = "Latin1"
-  show Binary = "Binary"
   show Hex = "Hex"
 
 -- | Convert an `Encoding` to a `String` in the format expected by Node.js
@@ -34,10 +36,9 @@ encodingToNode :: Encoding -> String
 encodingToNode ASCII = "ascii"
 encodingToNode UTF8 = "utf8"
 encodingToNode UTF16LE = "utf16le"
-encodingToNode UCS2 = "ucs2"
 encodingToNode Base64 = "base64"
+encodingToNode Base64Url = "base64url"
 encodingToNode Latin1 = "latin1"
-encodingToNode Binary = "binary"
 encodingToNode Hex = "hex"
 
 foreign import byteLengthImpl :: Fn2 (String) (String) (Int)
