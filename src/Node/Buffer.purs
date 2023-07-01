b5958565554eb110b1224d067b25dc4257cbee3e
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-07-01 08:59:32 -0500

    Drop unicode

--- src/Node/Buffer.purs
+++ src/Node/Buffer.purs
@@ -67,71 +67,71 @@ instance mutableBufferEffect :: MutableBuffer Buffer Effect where
   copy = copy
   fill = fill
 
-create ∷ Int → Effect Buffer
+create :: Int -> Effect Buffer
 create = Internal.create
 
-freeze ∷ Buffer → Effect ImmutableBuffer
+freeze :: Buffer -> Effect ImmutableBuffer
 freeze = Internal.copyAll
 
-unsafeFreeze ∷ Buffer → Effect ImmutableBuffer
+unsafeFreeze :: Buffer -> Effect ImmutableBuffer
 unsafeFreeze = Internal.unsafeFreeze
 
-thaw ∷ ImmutableBuffer → Effect Buffer
+thaw :: ImmutableBuffer -> Effect Buffer
 thaw = Internal.copyAll
 
-unsafeThaw ∷ ImmutableBuffer → Effect Buffer
+unsafeThaw :: ImmutableBuffer -> Effect Buffer
 unsafeThaw = Internal.unsafeThaw
 
-fromArray ∷ Array Int → Effect Buffer
+fromArray :: Array Int -> Effect Buffer
 fromArray = Internal.fromArray
 
-fromString ∷ String → Encoding → Effect Buffer
+fromString :: String -> Encoding -> Effect Buffer
 fromString = Internal.fromString
 
-fromArrayBuffer ∷ ArrayBuffer → Effect Buffer
+fromArrayBuffer :: ArrayBuffer -> Effect Buffer
 fromArrayBuffer = Internal.fromArrayBuffer
 
-toArrayBuffer ∷ Buffer → Effect ArrayBuffer
+toArrayBuffer :: Buffer -> Effect ArrayBuffer
 toArrayBuffer = Internal.toArrayBuffer
 
-read ∷ BufferValueType → Int → Buffer → Effect Number
+read :: BufferValueType -> Int -> Buffer -> Effect Number
 read = Internal.read
 
-readString ∷ Encoding → Int → Int → Buffer → Effect String
+readString :: Encoding -> Int -> Int -> Buffer -> Effect String
 readString = Internal.readString
 
-toString ∷ Encoding → Buffer → Effect String
+toString :: Encoding -> Buffer -> Effect String
 toString = Internal.toString
 
-write ∷ BufferValueType → Number → Int → Buffer → Effect Unit
+write :: BufferValueType -> Number -> Int -> Buffer -> Effect Unit
 write = Internal.write
 
-writeString ∷ Encoding → Int → Int → String → Buffer → Effect Int
+writeString :: Encoding -> Int -> Int -> String -> Buffer -> Effect Int
 writeString = Internal.writeString
 
-toArray ∷ Buffer → Effect (Array Int)
+toArray :: Buffer -> Effect (Array Int)
 toArray = Internal.toArray
 
-getAtOffset ∷ Int → Buffer → Effect (Maybe Int)
+getAtOffset :: Int -> Buffer -> Effect (Maybe Int)
 getAtOffset = Internal.getAtOffset
 
-setAtOffset ∷ Int → Int → Buffer → Effect Unit
+setAtOffset :: Int -> Int -> Buffer -> Effect Unit
 setAtOffset = Internal.setAtOffset
 
-slice ∷ Int → Int → Buffer → Buffer
+slice :: Int -> Int -> Buffer -> Buffer
 slice = Internal.slice
 
-size ∷ Buffer → Effect Int
+size :: Buffer -> Effect Int
 size = Internal.size
 
-concat ∷ Array Buffer → Effect Buffer
+concat :: Array Buffer -> Effect Buffer
 concat = Internal.concat
 
-concat' ∷ Array Buffer → Int → Effect Buffer
+concat' :: Array Buffer -> Int -> Effect Buffer
 concat' = Internal.concat'
 
-copy ∷ Int → Int → Buffer → Int → Buffer → Effect Int
+copy :: Int -> Int -> Buffer -> Int -> Buffer -> Effect Int
 copy = Internal.copy
 
-fill ∷ Int → Int → Int → Buffer → Effect Unit
+fill :: Int -> Int -> Int -> Buffer -> Effect Unit
 fill = Internal.fill
--- src/Node/Buffer/ST.purs
+++ src/Node/Buffer/ST.purs
@@ -73,71 +73,71 @@ instance mutableBufferST :: MutableBuffer (STBuffer h) (ST h) where
   copy = copy
   fill = fill
 
-create ∷ ∀ (h ∷ Region). Int → ST h (STBuffer h)
+create :: forall (h :: Region). Int -> ST h (STBuffer h)
 create = Internal.create
 
-freeze ∷ ∀ (h ∷ Region). STBuffer h → ST h ImmutableBuffer
+freeze :: forall (h :: Region). STBuffer h -> ST h ImmutableBuffer
 freeze = Internal.copyAll
 
-unsafeFreeze ∷ ∀ (h ∷ Region). STBuffer h → ST h ImmutableBuffer
+unsafeFreeze :: forall (h :: Region). STBuffer h -> ST h ImmutableBuffer
 unsafeFreeze = Internal.unsafeFreeze
 
-thaw ∷ ∀ (h ∷ Region). ImmutableBuffer → ST h (STBuffer h)
+thaw :: forall (h :: Region). ImmutableBuffer -> ST h (STBuffer h)
 thaw = Internal.copyAll
 
-unsafeThaw ∷ ∀ (h ∷ Region). ImmutableBuffer → ST h (STBuffer h)
+unsafeThaw :: forall (h :: Region). ImmutableBuffer -> ST h (STBuffer h)
 unsafeThaw = Internal.unsafeThaw
 
-fromArray ∷ ∀ (h ∷ Region). Array Int → ST h (STBuffer h)
+fromArray :: forall (h :: Region). Array Int -> ST h (STBuffer h)
 fromArray = Internal.fromArray
 
-fromString ∷ ∀ (h ∷ Region). String → Encoding → ST h (STBuffer h)
+fromString :: forall (h :: Region). String -> Encoding -> ST h (STBuffer h)
 fromString = Internal.fromString
 
-fromArrayBuffer ∷ ∀ (h ∷ Region). ArrayBuffer → ST h (STBuffer h)
+fromArrayBuffer :: forall (h :: Region). ArrayBuffer -> ST h (STBuffer h)
 fromArrayBuffer = Internal.fromArrayBuffer
 
-toArrayBuffer ∷ ∀ (h ∷ Region). STBuffer h → ST h ArrayBuffer
+toArrayBuffer :: forall (h :: Region). STBuffer h -> ST h ArrayBuffer
 toArrayBuffer = Internal.toArrayBuffer
 
-read ∷ ∀ (h ∷ Region). BufferValueType → Int → STBuffer h → ST h Number
+read :: forall (h :: Region). BufferValueType -> Int -> STBuffer h -> ST h Number
 read = Internal.read
 
-readString ∷ ∀ (h ∷ Region). Encoding → Int → Int → STBuffer h → ST h String
+readString :: forall (h :: Region). Encoding -> Int -> Int -> STBuffer h -> ST h String
 readString = Internal.readString
 
-toString ∷ ∀ (h ∷ Region). Encoding → STBuffer h → ST h String
+toString :: forall (h :: Region). Encoding -> STBuffer h -> ST h String
 toString = Internal.toString
 
-write ∷ ∀ (h ∷ Region). BufferValueType → Number → Int → STBuffer h → ST h Unit
+write :: forall (h :: Region). BufferValueType -> Number -> Int -> STBuffer h -> ST h Unit
 write = Internal.write
 
-writeString ∷ ∀ (h ∷ Region). Encoding → Int → Int → String → STBuffer h → ST h Int
+writeString :: forall (h :: Region). Encoding -> Int -> Int -> String -> STBuffer h -> ST h Int
 writeString = Internal.writeString
 
-toArray ∷ ∀ (h ∷ Region). STBuffer h → ST h (Array Int)
+toArray :: forall (h :: Region). STBuffer h -> ST h (Array Int)
 toArray = Internal.toArray
 
-getAtOffset ∷ ∀ (h ∷ Region). Int → STBuffer h → ST h (Maybe Int)
+getAtOffset :: forall (h :: Region). Int -> STBuffer h -> ST h (Maybe Int)
 getAtOffset = Internal.getAtOffset
 
-setAtOffset ∷ ∀ (h ∷ Region). Int → Int → STBuffer h → ST h Unit
+setAtOffset :: forall (h :: Region). Int -> Int -> STBuffer h -> ST h Unit
 setAtOffset = Internal.setAtOffset
 
-slice ∷ ∀ (h ∷ Region). Int → Int → STBuffer h → STBuffer h
+slice :: forall (h :: Region). Int -> Int -> STBuffer h -> STBuffer h
 slice = Internal.slice
 
-size ∷ ∀ (h ∷ Region). STBuffer h → ST h Int
+size :: forall (h :: Region). STBuffer h -> ST h Int
 size = Internal.size
 
-concat ∷ ∀ (h ∷ Region). Array (STBuffer h) → ST h (STBuffer h)
+concat :: forall (h :: Region). Array (STBuffer h) -> ST h (STBuffer h)
 concat = Internal.concat
 
-concat' ∷ ∀ (h ∷ Region). Array (STBuffer h) → Int → ST h (STBuffer h)
+concat' :: forall (h :: Region). Array (STBuffer h) -> Int -> ST h (STBuffer h)
 concat' = Internal.concat'
 
-copy ∷ ∀ (h ∷ Region). Int → Int → STBuffer h → Int → STBuffer h → ST h Int
+copy :: forall (h :: Region). Int -> Int -> STBuffer h -> Int -> STBuffer h -> ST h Int
 copy = Internal.copy
 
-fill ∷ ∀ (h ∷ Region). Int → Int → Int → STBuffer h → ST h Unit
+fill :: forall (h :: Region). Int -> Int -> Int -> STBuffer h -> ST h Unit
 fill = Internal.fill
