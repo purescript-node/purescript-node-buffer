bb0870df0aa1a3206435d32ebca65c49d70683a2
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-07-05 13:32:51 -0500

    Revert "Drop aliased encodings"
    This reverts commit e8fe84c06a0fd0ad3de58c4f44befd2afef1758c.

--- src/Node/Encoding.purs
+++ src/Node/Encoding.purs
@@ -12,18 +12,22 @@ data Encoding
   = ASCII
   | UTF8
   | UTF16LE
+  | UCS2
   | Base64
   | Base64Url
   | Latin1
+  | Binary
   | Hex
 
 instance showEncoding :: Show Encoding where
   show ASCII = "ASCII"
   show UTF8 = "UTF8"
   show UTF16LE = "UTF16LE"
+  show UCS2 = "UCS2"
   show Base64 = "Base64"
   show Base64Url = "Base64Url"
   show Latin1 = "Latin1"
+  show Binary = "Binary"
   show Hex = "Hex"
 
 -- | Convert an `Encoding` to a `String` in the format expected by Node.js
@@ -32,9 +36,11 @@ encodingToNode :: Encoding -> String
 encodingToNode ASCII = "ascii"
 encodingToNode UTF8 = "utf8"
 encodingToNode UTF16LE = "utf16le"
+encodingToNode UCS2 = "ucs2"
 encodingToNode Base64 = "base64"
 encodingToNode Base64Url = "base64url"
 encodingToNode Latin1 = "latin1"
+encodingToNode Binary = "binary"
 encodingToNode Hex = "hex"
 
 foreign import byteLengthImpl :: Fn2 String String Int
